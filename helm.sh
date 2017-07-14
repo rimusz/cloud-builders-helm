197a647eb6d32f276064f652a444c00cb8fd94ce
Rimas <rmocius@gmail.com>
2017-07-14 19:32:37 +0100

    add optional repo use and inprove the example

--- helm.sh
+++ helm.sh
@@ -6,10 +6,11 @@ sed -i '/cmd-/d' /workspace/.kube/config
 echo "Running: helm init --client-only"
 helm init --client-only
 
-echo "Adding chart helm repo"
-# Update the command below with your Helm repo if you want
-# it automaticly be available for use in pipeline steps
-helm repo add rimusz https://rimusz.github.io/charts/
+# check if repo values provided then add that repo if it is
+if [[ -z $HELM_REPO_NAME || -z $HELM_REPO_URL ]]
+  echo "Adding chart helm repo $HELM_REPO_URL "
+  helm repo add $HELM_REPO_NAME $HELM_REPO_URL
+fi
 
 echo "Running: helm repo update"
 helm repo update
