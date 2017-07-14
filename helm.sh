3f299611c52f5f455b2b73e2708685639536d927
Rimas Mocevicius <rimusz@users.noreply.github.com>
2017-07-14 18:15:46 +0100

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -6,10 +6,10 @@ sed -i '/cmd-/d' /workspace/.kube/config
 echo "Running: helm init --client-only"
 helm init --client-only
 
-# Uncomment this line and update with your Helm repo if you want
+echo "Adding chart helm repo"
+# Update the command below with your Helm repo if you want
 # it automaticly be available for use in pipeline steps
-#echo "Adding your chart helm repo"
-#helm repo add some-repo https://some-repo-charts.storage.googleapis.com
+helm repo add rimusz https://rimusz.github.io/charts/
 
 echo "Running: helm repo update"
 helm repo update
